ae8d93727fff70bd1eecf2a863e7940ba980d1ed
Marco Marin <marcomarin44@gmail.com>
2017-03-08 14:29:30 -0800

    Add new employee questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+person_name = gets.chomp 
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born?"
+year = gets.chomp 
+
+puts "Should we order some garlic for you?"
+garlic = gets.chomp 
+
+puts "Would you like to enroll in the company's health insurance?"
+insurance = gets.chomp 
