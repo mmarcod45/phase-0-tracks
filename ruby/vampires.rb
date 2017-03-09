e9103d542d62abc3ce4390dc9e3fb6049525d07c
Marco Marin <marcomarin44@gmail.com>
2017-03-08 19:28:49 -0800

    Add conditional logic and take user input

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,14 +1,72 @@
-puts "What is your name?"
-person_name = gets.chomp 
+puts "How many employees will be processed?"
+number_processed = gets.chomp.to_i
 
-puts "How old are you?"
-age = gets.chomp
+current_count = number_processed 
+until current_count == 0
 
-puts "What year were you born?"
-year = gets.chomp 
 
-puts "Should we order some garlic for you?"
-garlic = gets.chomp 
+	puts "What's your name?"
+	person_name = gets.chomp 
+	
+	puts "How old are you?"
+	age = gets.chomp.to_i
+	
+	puts "What year were you born?"
+	year_born = gets.chomp.to_i
+	
+	puts "What age did you turn or will be turning this year?"
+	age_turning = gets.chomp.to_i 
+	
+	current_year = age_turning + year_born
+	
+	puts "Should we order garlic bread for you?"
+	garlic_bread = gets.chomp 
+	
+	puts "Would you like to enroll in the company's insurance?"
+	insurance = gets.chomp 
+	
+	
+	
+	if person_name == "Drake Cula"
+		puts "Definitely a vampire"
+	elsif person_name == "Tu Fang"
+		puts "Definitely a vampire"
+	elsif current_year == 2017 && garlic_bread == "y" && insurance == "y"
+		puts "Probably not a vampire"
+	elsif current_year != 2017 && (garlic_bread == "n" && insurance == "n")
+		puts "Almost certainly a vampire"
+	elsif current_year != 2017 && (garlic_bread == "n" || insurance == "n")
+		puts "Probably a vampire"
+	else
+		puts "Results are inconclusive"
+	end
 
-puts "Would you like to enroll in the company's health insurance?"
-insurance = gets.chomp 
+
+	current_count = current_count - 1
+end 
+
+
+
+#puts "It is #{current_year}"
+
+
+#if current_year == "2017" && (garlic_bread == "y" || insurance == "y")
+#	puts "Probably not a vampire"
+#elsif current_year != "2017" && (garlic_bread == "n" || insurance == "n")
+#	puts "Probably a vampire"
+#elsif current_year != "2017" && garlic_bread == "n" && insurance == "n"
+#	puts "Almost certainly a vampire"
+#else 
+#	puts "Results inconclusive"
+#end 
+
+#elsif person_name == "Drake Cula"
+#	puts "Definitely a vampire"
+#elsif person_name == "Tu Fang"
+#	puts "Definitely a vampire"
+#else
+#	puts "Results inconclusive"
+#end
+
+
+
