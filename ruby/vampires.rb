58ad9e6fc04a0e4adbe6f9fd7aabd15db0393d97
Marco Marin <marcomarin44@gmail.com>
2017-03-12 12:22:50 -0700

    Add conditional logic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,35 +1,60 @@
-
-secret_password = "marco"
-
 def encrypt(word)
-	index = 0 
-	while index < word.length 
-	  word[index] = word[index].next
-	  index += 1 
-	end 
-	word
+  index = 0 
+  while index < word.length 
+  if word[index]=="z"
+  word[index] = "a"
+  else
+  word[index] = word[index].next
+  end
+
+  index += 1 
+  end 
+  word
 end
 
-puts encrypt(secret_password) #should this be driver test to encrypt? just change secret_password to "marco"
-#secret_password = encrypt("marco") #also, uncomment this line for drvier code?
-
 def decrypt(word)
-	index = 0 
-	#secret_password[index] = "#{secret_password}"
-	
-	#letter = "abcdefghijklmnopqrstuvwxyz".index("#{secret_password[index]}") - 1
-	
-	alphabet = "abcdefghijklmnopqrstuvwxyz"
-	while index < word.length 
-	  letter = word[index]
-	  position = alphabet.index(letter)
-	  new_position = position - 1 
-	  word[index] = alphabet[new_position]
-	  index += 1 
-	end 
-	word 
+  index = 0 
+ #secret_password[index] = "#{secret_password}"
+   
+ #letter = "abcdefghijklmnopqrstuvwxyz".index("#{secret_password[index]}") - 1
+   
+ alphabet = "abcdefghijklmnopqrstuvwxyz"
+  while index < word.length 
+  letter = word[index]
+  position = alphabet.index(letter)
+  new_position = position - 1 
+  word[index] = alphabet[new_position]
+  index += 1 
+  end 
+  word
 end 
 
-puts decrypt(secret_password)
-# use the same logic as comments on lines 12 & 13 for the driver code for decryption?
+#decrypt(encrypt("swordfish"))
+
+#Nested Method Call:
+#Ruby evaluates the methods one after another. Since encrypt is in parentheses
+#it evaluates that and it passes the value to decrypt.
+#This will work if both methods have already been defined.
+
+puts "Would you like to encrypt or decrypt a password?"
+user_input = gets.chomp 
+
+
+if user_input == "encrypt"
+	puts "Type in the password?"
+	code_word = gets.chomp 
+	puts encrypt(code_word)
+elsif user_input == "decrypt"
+	puts "Type in the password?"
+	code_word = gets.chomp 
+	puts decrypt(code_word)
+else
+	puts "Invalid input"
+end 
+
+
+
+
+
+
 
