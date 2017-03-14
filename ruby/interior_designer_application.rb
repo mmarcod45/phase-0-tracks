325094ce8bea0b72b5e68809a3c2588a0463cb14
Marco Marin <marcomarin44@gmail.com>
2017-03-13 20:52:52 -0700

    Add keys to hash - interior designer application

---
+++ ruby/interior_designer_application.rb
@@ -0,0 +1,14 @@
+
+applicant = {
+	"name" => "Marco",
+	"address" => "444 Main Street",
+	"email" => "mmarin@gmail.com",
+	"phone" => "777-777-7777",
+	"fave shade of blue" => "cerulean",
+	"wall paper preferences" => "Paisley", 
+	"ombre is" => "Fierce", 
+}
+
+applicant["name"]
+applicant["address"]
+applicant["email"] 
