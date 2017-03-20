e5ded2abb1ac26c542dde9b4c9382ede491f5c9f
Marco Marin <marcomarin44@gmail.com>
2017-03-20 12:06:10 -0700

    Design for Puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+
+
+CLASS (SPECIES) ------------------------
+Puppy
+
+CHARACTERISTICS ------------------------
+Color:
+Weight:
+Breed: 
+Eye color:
+Friendly: yes 
+
+BEHAVIOR ------------------------
+Playful 
+Licks 
+Obedient 
+
+
+
+
