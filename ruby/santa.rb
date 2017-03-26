be45b132f8097f68806f9be10648c86a7af33d66
Marco Marin <marcomarin44@gmail.com>
2017-03-26 14:15:32 -0700

    Transfer code to santa file

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,46 +1,70 @@
+
 class Santa 
+
+ 	attr_accessor :gender 
+ 	attr_reader :age, :ethnicity
+
 	def initialize (gender, ethnicity)
 		puts "Initializing Santa instance..."
-		@gender = gender # these are called instance variables @gender
+		@gender = gender 
 		@ethnicity = ethnicity
 		puts "Gender: #{@gender}, Ethnicity: #{@ethnicity}"
 		@age = 0 
-		@reindeer = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"].shuffle
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 	end 
 
-	def speak #these are Instance Methods
+	def speak 
 		puts "Ho, ho, ho! Haaaapy holidays!"
 	end 
 
 	def eat_milk_and_cookies(cookie_type)
 		puts "That was a good #{cookie_type} cookie!"
 	end  
+
+	def celebrate_birthday
+	  @age += 1 
+	end 
+	
+	def get_mad_at(reindeer_name)  
+		@reindeer_ranking.index(reindeer_name)
+	  index = @reindeer_ranking.index(reindeer_name)
+	  @reindeer_ranking << reindeer_name
+	  @reindeer_ranking.delete_at(index)
+	  p @reindeer_ranking 
+	end 
+
 end 
 
-santa = Santa.new("Male", "White")  # This initializes a new Instance of our Santa class
-santa.speak
+santa = Santa.new("Male", "N/A")  
 #santa.speak
 
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+#   puts "There are now #{santas.length} Santa instances in the array"
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
 
-# rand(1..20)
-# Santa.new - returns a new instance of the Santa class (also called an object number) 
-# Santa.new.speak -
-# x = Santa.new.speak - returns a String data type 
+puts "This santa turns #{santa.celebrate_birthday} this year"
+santa.get_mad_at("Vixen")
+santa.gender = "new new gender"
 
-# must be the same initialized class with different attributes
+santa.age 
+p santa.age
 
+santa.ethnicity
+p santa.ethnicity 
 
-# use these notes: 
-# def celebrate_birthday 
-# @age + 1  
-# end  
+p santa
 
-# def get_mad(reindeer_name) ??
-# 	@reindeer << reindeer_ranking ??
-# 	p @reindeer 
-# end 
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 
 
-
+20.times do 
+  santa = Santa.new(example_genders.sample, example_genders.sample)
+  santa = rand(1..40)
+end 
 
 
