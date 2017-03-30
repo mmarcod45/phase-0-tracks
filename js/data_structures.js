339aaffbc640bc4d36f964908da62221ba5ba674
Marco Marin <marcomarin44@gmail.com>
2017-03-29 21:42:52 -0700

    Add state_data file to be able to access it

---
+++ js/data_structures.js
@@ -0,0 +1,42 @@
+var colors = ["red", "blue", "yellow", "orange"];
+var horses = ["Tom", "Ed", "Becky", "Gary"];
+
+colors.push("brown");
+horses.push("Sally");
+
+// console.log(colors);
+// console.log(horses);
+
+
+var horse_colors = {}
+for (var i = 0; i < horses.length; i++) {
+	horse_colors[horses[i]]=colors[i];
+}
+
+console.log(horse_colors);
+
+
+////////
+
+function Car(make, model, fuel_efficient) {
+	console.log("We are creating multiple cars...");
+	console.log("new car", this);
+	this.make = make
+	this.model = model
+	this.fuel_efficient = fuel_efficient
+
+	this.honk = function() { console.log("Beep!"); };
+}
+
+var first_car = new Car("Bugatti", "Veyron", true);
+
+console.log(first_car);
+console.log("Our car can beep!");
+first_car.honk(); 
+
+var second_car = new Car("Mercedes", "E Series", true);
+
+console.log(second_car);
+console.log("Our car can beep!");
+second_car.honk();
+
