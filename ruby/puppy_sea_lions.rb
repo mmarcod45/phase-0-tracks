85ef0d59062bda30459745dcb5c485a12a5306d7
Marco Marin <marcomarin44@gmail.com>
2017-04-11 13:30:26 -0700

    Comment out the Puppy class description

--- ruby/puppy_sea_lions.rb
+++ ruby/puppy_sea_lions.rb
@@ -1,14 +1,14 @@
-CLASS (SPECIES) ------------------------
-Puppy
+# CLASS (SPECIES) ------------------------
+# Puppy
 
-CHARACTERISTICS ------------------------
-Color: Brown
-Weight: 42oz
-Breed: Husky
-Eye color: Green
-Friendly: Yes 
+# CHARACTERISTICS ------------------------
+# Color: Brown
+# Weight: 42oz
+# Breed: Husky
+# Eye color: Green
+# Friendly: Yes 
 
-BEHAVIOR ------------------------
-Playful 
-Licks 
-Obedient 
+# BEHAVIOR ------------------------
+# Playful 
+# Licks 
+# Obedient 
