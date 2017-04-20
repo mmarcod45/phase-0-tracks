340db1c5de787c29670aad97cea6a9cf10e190ff
Marco Marin <marcomarin44@gmail.com>
2017-04-20 10:47:31 -0700

    Add example code of a while loop to get different results

---
+++ js/explore_sea_lions.js
@@ -0,0 +1,24 @@
+// release 3 
+
+for (var i=1; i <= 10; i++ ) { // this will print all numbers starting at 1
+	console.log(i);
+}
+
+for (var i=1; i <= 10; i+=2 ) { // this will increment by 2 starting at 1
+	console.log(i);
+}
+
+for (var i=0; i <= 10; i+=2 ) { // this will increment by 2 starting at 0
+	console.log(i);
+}
+
+for (var i=10; i >= 0; i-- ) { // this will count down and stop at 0
+	console.log(i);
+}
+
+for (var i=10; i > 0; i-- ) { // this will count down and stop at 1
+	console.log(i);
+}
+
+
+// release 4
